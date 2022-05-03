a86809e42eea2fa4da8433ca677a4576c6eec961
Isabelle Lafont <isalafont@gmail.com>
2022-05-03 18:07:57 +0200

    Add Search components
    create search bar
    with axios get the API Wikipedia
    then display results

---
+++ src/components/Search.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Search = () => {
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get(
+        'https://en.wikipedia.org/w/api.php',
+        {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term,
+          },
+        }
+      );
+      setResults(data.query.search);
+    };
+
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+        </div>
+        {result.snippet}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
